acb29608af22a2bed8a0ee4105099753cff80fd7
Anusha Bethini <anushabethini@gmail.com>
2024-10-06 23:07:42 -0400

    time change

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/api/login', (req, res) => {
 
     for (let user of users) {
         if (username == user.username && password == user.password) {
-            let token = jwt.sign({ id: user.id, username: user.username }, secretkey, { expiresIn: '3m });
+            let token = jwt.sign({ id: user.id, username: user.username }, secretkey, { expiresIn: '3m' });
             res.json({
                 success: true,
                 err: null,
